334617f63247b2d5ec381a7fc1ae8713aeb5e33d
tojiddin12 <madrahimovtojidin@gmail.com>
2023-04-01 14:34:52 +0500

    Javob 2

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,11 +3,13 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_One = 1;
+        final int VARIABLE_ONE = 1;
         int variableTwo = 2;
-        int variable_three = 3;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+        final int VARIABLE_THREE = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
+        int variableSix = 6;
+        int variableSeven = 7;
+        int variableEight = 8;
     }
 }
